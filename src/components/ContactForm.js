a5e511ab9943b5ae09cdf20d005ba09dd5dabf89
ridwan <ridwanediallo99@gmail.com>
2023-10-15 21:36:33 +0000

    Update contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,23 +1,23 @@
-import React from 'react'
+import React from 'react';
+import contactImage from '../images/ridwan.jpg';
 
 const ContactForm = () => {
   return (
-    <section className="text-gray-600 body-font relative">
-      <div className="container px-5 py-24 mx-auto">
-        <div className="flex flex-col text-center w-full mb-12">
-          <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
-            Contact Us
-          </h1>
-          <p className="lg:w-2/3 mx-auto leading-relaxed text-black">
-            Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
-            gentrify.
-          </p>
-        </div>
-        <div className="lg:w-1/2 md:w-2/3 mx-auto">
+    <section>
+      <div className=" text-center mt-28 mb-14">
+        <h1 className="text-xl">Contact me</h1> <br />
+      </div>
+      <div className="flex flex-wrap justify-center text-gray-600 body-font relative">
+        <div className="container flex flex-wrap shadow-lg">
+          <img
+            src={contactImage}
+            alt="Image"
+            class="md:w-2/5 rounded-l-lg object-cover mr-4"
+          />
           <form
             action="https://formspree.io/f/xwkyvenp"
             method="post"
-            className="flex flex-wrap -m-2"
+            className="flex flex-wrap -m-2 md:w-3/5"
           >
             <div className="p-2 w-1/2">
               <div className="relative">
@@ -78,6 +78,18 @@ const ContactForm = () => {
       </div>
     </section>
   );
-}
+};
 
-export default ContactForm
+export default ContactForm;
+
+
+      // <div className="container px-5 py-24 mx-auto">
+      //   <div className="flex flex-col text-center w-full mb-12">
+      //     <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+      //       Contact Us
+      //     </h1>
+      //     <p className="lg:w-2/3 mx-auto leading-relaxed text-black">
+      //       Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
+      //       gentrify.
+      //     </p>
+      //   </div>
