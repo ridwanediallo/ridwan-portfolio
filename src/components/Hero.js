f3ee6f9b381574356ffc927358ca22bd7b2eb431
ridwan <ridwanediallo99@gmail.com>
2023-11-16 21:13:17 +0000

    Update contact me button style

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ import image from '../images/undraw_Developer_activity_re_39tg.png'
 
 const Hero = () => {
   return (
-    <section className="text-gray-600 body-font bg-black">
+    <section className="text-gray-600 body-font bg-black lg:py-24">
       <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
           <img
@@ -32,7 +32,7 @@ const Hero = () => {
             various web frameworks such as Ruby on Rails, React, and Express.
           </p>
           <div className="flex justify-center">
-            <button className="inline-flex text-white bg-blue-600 border-0 py-2 px-6 focus:outline-none hover:bg-transparent rounded text-lg">
+            <button className="inline-flex text-white bg-blue-600 hover:border py-2 px-6 focus:outline-none hover:bg-transparent hover:border-blue-600 rounded text-lg">
               Contact me
             </button>
             <a href="https://github.com/ridwanediallo">
