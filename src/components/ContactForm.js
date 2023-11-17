defb9252d1ed9746221ba403ab3ad1f3bda852c9
ridwan <ridwanediallo99@gmail.com>
2023-11-17 23:04:22 +0000

    Remove commented code from contact form component

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -84,15 +84,3 @@ const ContactForm = () => {
 };
 
 export default ContactForm;
-
-
-      // <div className="container px-5 py-24 mx-auto">
-      //   <div className="flex flex-col text-center w-full mb-12">
-      //     <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
-      //       Contact Us
-      //     </h1>
-      //     <p className="lg:w-2/3 mx-auto leading-relaxed text-black">
-      //       Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
-      //       gentrify.
-      //     </p>
-      //   </div>
