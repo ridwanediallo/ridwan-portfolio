f3494b291325aec00e4f832be6b2a0198dcfdd95
ridwan <ridwanediallo99@gmail.com>
2023-11-20 23:53:42 +0000

    Add download resume feature

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 import { VscGithub } from 'react-icons/vsc';
 import { FaLinkedin, FaTwitter } from 'react-icons/fa';
+import resumePDF from '../resume.pdf';
 
 import image from '../images/undraw_Developer_activity_re_39tg.png'
 
 const Hero = () => {
+
+    const downloadResume = () => {
+      window.open(resumePDF, '_blank');
+    };
+
   return (
     <section className="text-gray-600 body-font bg-black lg:py-24">
       <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
@@ -18,7 +24,7 @@ const Hero = () => {
         </div>
         <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi <span className="animate-wave inline-block">👋</span>!
+            Hi! <span className="animate-wave inline-block">👋</span>
             <br className="hidden lg:inline-block" />I am Ridwan,
           </h1>
           <p className="mb-8  text-white text-base body-font">
@@ -32,8 +38,8 @@ const Hero = () => {
             various web frameworks such as Ruby on Rails, React, and Express.
           </p>
           <div className="flex justify-center">
-            <button className="inline-flex text-white bg-blue-600 hover:border py-2 px-6 focus:outline-none hover:bg-transparent hover:border-blue-600 rounded text-lg">
-              Contact me
+            <button onClick={downloadResume} className="inline-flex text-white bg-blue-600 hover:border py-2 px-6 focus:outline-none hover:bg-transparent hover:border-blue-600 rounded text-lg">
+              Hire me
             </button>
             <a href="https://github.com/ridwanediallo">
               <VscGithub className="text-4xl mx-6 mt-1" />
