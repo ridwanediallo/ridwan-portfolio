4377a4723c003a1337f308f3ee085cc54c2b0eb5
ridwan <ridwanediallo99@gmail.com>
2023-11-20 18:03:29 +0000

    Add testimonies array

---
+++ src/testimonies.js
@@ -0,0 +1,42 @@
+const testimonies = [
+  {
+    id: 1,
+    name: `Emirjeta Veisllari`,
+    title: 'Full-Stack web Developer',
+    image: '/images/rohit.png',
+    testimony: `I have come to know Ridwan as a very smart, responsible, hard-working, empathic, and caring person. I admire his work ethic and the knowledge he gathers about every topic in the field we are studying. You can always count on him for help and advice. He will be a very qualitative individual to add to any team. I highly recommend him!`,
+  },
+  {
+    id: 2,
+    name: 'Brahim Didi',
+    title: 'Full-Stack Developer',
+    image: '/images/rohit.png',
+    testimony:
+      'Ridwan and I worked together on a tough group project, and I was impressed by how calm and composed he is even in critical moments, He is also very dedicated and a true professional. He is a perfectionist and hard worker, I was very pleased to collaborate with him.',
+  },
+  {
+    id: 3,
+    name: 'Adekunle Ismail',
+    title: 'Web Developer',
+    image: '/images/rohit.png',
+    testimony: `I had the chance to work with Ridwan on different projects. His technical skills were always impressive. He constantly showed professional behaviour and a high level of problem-solving skills.
+       He is always creative with writing codes and shows a nice attitude towards team members. Based on that, I would gladly recommend him as a software developer with his technical and soft skills.`,
+  },
+  {
+    id: 4,
+    name: `Said Rašinlić`,
+    title: 'Software Developer',
+    image: '/images/rohit.png',
+    testimony: `As someone who has worked with Ridwan on several occasions, I can relate to the simplicity with which he accomplished assignments. A talented guy with a strong work ethic and excellent soft skills who is a breath of fresh air for the team. A punctual, responsible individual who is always willing to assist and get to work without any excuses. I'm looking forward to taking on bigger projects in the future.`,
+  },
+  {
+    id: 5,
+    name: 'Mohammed Mahdi',
+    title: 'Full-Stack Developer',
+    image: '/images/rohit.png',
+    testimony:
+      'Ridwan is a hard-working student and can easily adapt to the environment. We have worked countless times during our Microverse studies. We have worked in pair programming together and worked on several projects. He is a quick learner and knowledgeable in terms of Web Development and problem-solving. He would be an important member of any team since his skills are up to the industry standards.',
+  },
+];
+
+export default testimonies;
