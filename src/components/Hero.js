fdd09b1c7d1b89cfad48cdbe9fffd83b86223fd9
ridwan <ridwanediallo99@gmail.com>
2023-11-23 17:28:44 +0000

    Use useTranslation to translate Hero content

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,13 @@
 import React from 'react';
-
 import { VscGithub } from 'react-icons/vsc';
+import { useTranslation } from 'react-i18next';
 import { FaLinkedin, FaTwitter } from 'react-icons/fa';
 import resumePDF from '../resume.pdf';
 
 import image from '../images/undraw_Developer_activity_re_39tg.png'
 
 const Hero = () => {
-
+    const { t } = useTranslation();
     const downloadResume = () => {
       window.open(resumePDF, '_blank');
     };
@@ -24,22 +24,15 @@ const Hero = () => {
         </div>
         <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi! <span className="animate-wave inline-block">👋</span>
-            <br className="hidden lg:inline-block" />I am Ridwan,
+            {t('greeting')} <span className="animate-wave inline-block">👋</span>
+            <br className="hidden lg:inline-block" />{t('name')}
           </h1>
           <p className="mb-8  text-white text-base body-font">
-            a Full-Stack software engineer, currently working as Code
-            Reviewer At Microverse. I have more that one year of experience in
-            writing code. I am looking for a position as a backend, frontend, or
-            full-stack web developer. I am a dedicated and driven professional
-            who is eager to build, learn, grow, and be a valuable member of a
-            software development team. I have experience working with Ruby,
-            JavaScript, PostgreSQL, React, Git, Ruby on Rails, HTML5, CSS3, and
-            various web frameworks such as Ruby on Rails, React, and Express.
+            {t('about-me')}
           </p>
           <div className="flex justify-center">
             <button onClick={downloadResume} className="inline-flex text-white bg-blue-600 hover:border py-2 px-6 focus:outline-none hover:bg-transparent hover:border-blue-600 rounded text-lg">
-              Hire me
+              {t('hire-me')}
             </button>
             <a href="https://github.com/ridwanediallo">
               <VscGithub className="text-4xl mx-6 mt-1" />
