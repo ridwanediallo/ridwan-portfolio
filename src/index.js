61e57c43151d29a040e58ab861f8b2d4cc859ece
ridwan <ridwanediallo99@gmail.com>
2023-11-23 17:25:51 +0000

    Add feature to translate portfolio content into many languages

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
 import './index.css';
 import App from './App';
+
+
+import enTranslation from './locales/en.json';
+import frTranslation from './locales/fr.json';
+import arTranslation from './locales/ar.json';
+
+i18n.use(initReactI18next).init({
+  resources: {
+    en: { translation: enTranslation },
+    fr: { translation: frTranslation },
+    ar: { translation: arTranslation },
+  },
+  lng: 'en', // Default language
+  fallbackLng: 'en', // Fallback language
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
